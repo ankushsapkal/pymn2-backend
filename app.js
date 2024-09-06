9799608615a2a7d96efa36069706a22a78176c6d
aniket <aniketpatil108108@gmail.com>
2024-09-06 23:47:01 +0530

    Resolved CORS problem

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
